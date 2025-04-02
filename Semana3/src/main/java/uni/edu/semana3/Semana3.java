e070baff5863939985e69ebd6664e0afc034b920
Carlos <carlos.fernandez.i@uni.pe>
2025-04-01 19:57:10 -0500

    Semana 3

---
+++ Semana3/src/main/java/uni/edu/semana3/Semana3.java
@@ -0,0 +1,16 @@
+package uni.edu.semana3;
+
+public class Semana3 {
+
+    public static void main(String[] args) {
+        /*
+        1. Click derecho en uni.edu.semana3
+        2. New -> JFrame Form
+       
+        3. En el directorio ir a Project Files
+        4. Acceder a pom.xml
+        5. En la línea 11 va a estar uni.edu.semana3.NombreProyecto donde NombreProyecto es el nombre del proyecto que le has dado
+        6. Cambiar NombreProyecto por el nombre del interfaz
+        */
+    }
+}
